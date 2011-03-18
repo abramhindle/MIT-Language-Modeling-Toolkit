3dafacf127b014da46a228546a72ce91b9997349
Abram Hindle <abram.hindle@softwareprocess.es>
2011-03-17 21:25:27 -0700

    Well I solved the stupid heap problem. Now sorta works. Just need to guess the future better.

--- src/LiveGuess.cpp
+++ src/LiveGuess.cpp
@@ -105,96 +105,68 @@ std::auto_ptr< std::vector<LiveGuessResult> > LiveGuess::Predict( char * str, in
       //Logger::Log(0, "Word:\t%d\t%s\t%d\t\%d\t%e\n", j, sWord, vWordI, index, prob);
     }
     Logger::Log(0, "Final:\t%d\t%e\t%s\n", index, prob, sWord);
-    double probpred[predictions];
-    const int size = 10;
-    const VCompare cmp();
-    // const boost::identity_property_map ID();
-    //const VID id();
-    // const boost::typed_identity_property_map<long unsigned int> ID();
-    // boost::fibonacci_heap<VocabProb, std::less<VocabProb>, boost::identity_property_map >::fibonacci_heap(size , cmp, ID );
-    // boost::fibonacci_heap<VocabProb, std::less<VocabProb>, boost::typed_identity_property_map<long unsigned int> >::fibonacci_heap heap( size, cmp, id);
-    //boost::fibonacci_heap<VocabProb, VCompare>::fibonacci_heap heap( size, VCompare());
-    //boost::relaxed_heap<VocabProb, VCompare>::relaxed_heap heap( (const long unsigned int)10 );//, VCompare());
-    vector<VocabProb> heap(0);
-    make_heap (heap.begin(),heap.end());
-    //boost::fibonacci_heap<VocabProb> heap( size, cmp );    
-    const NgramVector & ngrams = _lm.model().vectors( _order );
-    const ProbVector & probabilities = _lm.probs( _order - 1  );
-    int count = 0;
-    for (int j = 0; j < vocab.size(); j++) {
-      NgramIndex newIndex = ngrams.Find( index, j);
-      Prob prob = probabilities[ newIndex ];
-      const VocabProb v(prob,j);
-      if ( count < size ) {
-        heap.push_back( v ); push_heap( heap.begin(), heap.end() );
-        count++;
-      } else if ( heap.front() < v ) {
-        // this is dumb
-        pop_heap (heap.begin(),heap.end());  heap.pop_back();
-        heap.push_back( v ); push_heap( heap.begin(), heap.end() );
-        // should we update?
-      }
-    }
-    sort_heap( heap.begin(), heap.end() ); // heap now is internally sorted 
-    for( int j = 0; j < heap.size(); j++) {
-      VocabProb v = heap[ j ];
-      Logger::Log(0, "%d\t%e\t%s\n", j, v.prob, vocab[ v.index ]);
-    }
 
   }
+
+  // now find the index of the last word if it was order - 1 ?
+  NgramIndex index = 0;
+  for (int i = 0; i < _order - 1; ++i) {
+    const ProbVector & probabilities = _lm.probs( i  );
+    const NgramVector & ngrams = _lm.model().vectors( i + 1   );
+    const char * sWord = words[ words.size() - (_order - 1) + i ];
+    VocabIndex vWordI = vocab.Find( sWord );
+    index = ngrams.Find(index, vWordI);
+    Prob prob = probabilities[ index ];
+    Logger::Log(0, "Word:\t%d\t%s\t%d\t\%d\t%e\n", i, sWord, vWordI, index, prob);    
+  }
+  double probpred[predictions];
+  const int size = 10;
+  // const VCompare cmp();
+  // const boost::identity_property_map ID();
+  //const VID id();
+  // const boost::typed_identity_property_map<long unsigned int> ID();
+  // boost::fibonacci_heap<VocabProb, std::less<VocabProb>, boost::identity_property_map >::fibonacci_heap(size , cmp, ID );
+  // boost::fibonacci_heap<VocabProb, std::less<VocabProb>, boost::typed_identity_property_map<long unsigned int> >::fibonacci_heap heap( size, cmp, id);
+  //boost::fibonacci_heap<VocabProb, VCompare>::fibonacci_heap heap( size, VCompare());
+  //boost::relaxed_heap<VocabProb, VCompare>::relaxed_heap heap( (const long unsigned int)10 );//, VCompare());
+  vector<VocabProb> heap(0);
+  // I hate the STL heaps
+  make_heap (heap.begin(),heap.end());
+  //boost::fibonacci_heap<VocabProb> heap( size, cmp );    
+  const NgramVector & ngrams = _lm.model().vectors( _order );
+  const ProbVector & probabilities = _lm.probs( _order - 1  );
+  int count = 0;
+  for (int j = 0; j < vocab.size(); j++) {
+    NgramIndex newIndex = ngrams.Find( index, j);
+    Prob prob = probabilities[ newIndex ];
+    const VocabProb v( -1 * prob,j);
+    if ( count < size ) {
+      heap.push_back( v ); //push_heap( heap.begin(), heap.end() );
+      count++;
+      if (count == size) {
+          make_heap (heap.begin(),heap.end());
+      }
+      // this is irritating, basically it means the highest rank stuff
+      // will be in the list and we only kick out the lowest ranked stuff
+      // (which will be the GREATEST of what is already there)
+    } else if ( -1 * heap.front().prob < prob ) {
+      // this is dumb        
+      pop_heap (heap.begin(),heap.end());  heap.pop_back();
+      heap.push_back( v ); push_heap( heap.begin(), heap.end() );
+      // should we update?
+    }
+  }
+  sort_heap( heap.begin(), heap.end() ); // heap now is internally sorted 
+  for( int j = 0; j < heap.size(); j++) {
+    VocabProb v = heap[ j ];
+    Logger::Log(0, "%d\t%e\t%s\n", j, log( -1 * v.prob ) / log(10), vocab[ v.index ]);
+  }
+
+  // left to do:
+  // - iterate over a few possibilities and chains
+  // - return those results
+
+
   std::auto_ptr< std::vector<LiveGuessResult> > returnValue( new std::vector<LiveGuessResult>() );
   return returnValue;
-  ///* stolen from Perplexity */
-  //{
-  //  // now make a fake file
-  //  std::vector< char * > zfileContents();
-  //  zfileContents.push_back( str );
-  //  FakeZFile corpusFile( zfileContents );
-  //
-  //  /* it's like the perplexity optimizer but in a function.. */
-  //  vector<CountVector> _probCountVectors();
-  //  vector<CountVector> _bowCountVectors();
-  //  size_t              _numOOV = 0;
-  //  size_t              _numWords = 0;
-  //  size_t              _numZeroProbs = 0;
-  //  size_t              _numCalls = 0;
-  //  double              _totLogProb = 0;
-  //  
-  //  
-  //
-  //  BitVector vocabMask(_lm.vocab().size(), 1);
-  //  _lm._pModel->LoadEvalCorpus(_probCountVectors, _bowCountVectors,
-  //                              vocabMask, corpusFile, _numOOV, _numWords);
-  //  
-  //  vector<BitVector> probMaskVectors(_order + 1);
-  //  vector<BitVector> bowMaskVectors(_order);
-  //  for (size_t o = 0; o <= _order; o++)
-  //    probMaskVectors[o] = (_probCountVectors[o] > 0);
-  //  for (size_t o = 0; o < _order; o++)
-  //    bowMaskVectors[o] = (_bowCountVectors[o] > 0);
-  //  SharedPtr<Mask>     _mask = _lm.GetMask(probMaskVectors, bowMaskVectors);
-  //  
-  //  /* now we're all initialized */
-  //
-  //  for (size_t o = 0; o <= _order; o++) {
-  //    // assert(alltrue(counts == 0 || probs > 0));
-  //    // _totLogProb += dot(log(probs), counts, counts > 0);
-  //    // _totLogProb += sum((log(probs) * counts)[counts > 0]);
-  //    const CountVector &counts(_probCountVectors[o]);
-  //    const ProbVector & probs(_lm.probs(o));
-  //    for (size_t i = 0; i < counts.length(); i++) {
-  //      if (counts[i] > 0) {
-  //        assert(std::isfinite(probs[i]));
-  //        if (probs[i] == 0)
-  //          _numZeroProbs++;
-  //        else
-  //          _totLogProb += log(probs[i]) * counts[i];
-  //      }
-  //    }
-  //  }
-  //
-  //  
-  //
-  //} /* close scope */
-  //
 }
